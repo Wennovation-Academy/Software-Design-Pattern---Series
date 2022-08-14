36448b9e0c57413460a67851298e425ac28a6b12
aliabbas101 <alia63581@gmail.com>
2022-08-14 18:54:18 +0500

    Added example code for Factory method pattern

--- Creational Design Patterns/Factory Method/example_1.ts
+++ Creational Design Patterns/Factory Method/example_1.ts
@@ -1,4 +1,4 @@
-export class Notification {
+export class WindowsNotification {
 
     public title: string;
     public text: string;
@@ -17,13 +17,58 @@ export class Notification {
         console.log("Windows Notification clicked");
         callback();
     }
-
     public onClosed(callback): void {
         console.log("Windows Notification Closed");
         callback();
     }
 }
 
+export class MacOSNotification {
+
+    public title: string;
+    public text: string;
+    public thumbnailURL: string;
+
+    constructor(title, text, thumbnailURL){
+        this.title = title;
+        this.text = text;
+        this.thumbnailURL = thumbnailURL;
+    }
+
+    public render() {
+        console.log("MacOS Notification Displayed with title: ", this.title);
+    }
+    public onClick(callback): void {
+        console.log("MacOS Notification clicked");
+        callback();
+    }
+    public onClosed(callback): void {
+        console.log("MacOS Notification Closed");
+        callback();
+    }
+}
+
+
 export class MainApplication {
     
+    private configuredOS = "windows";
+    constructor(){
+        this.greetUser();
+    }
+
+    private greetUser(){
+        let notification;
+
+        let title = "Welcome !";
+        let text = "Let's take a tour";
+        let thumbnailURL = "../assets/welcome.png";
+
+        if(this.configuredOS == "windows"){
+
+            notification = new WindowsNotification(title, text, thumbnailURL);
+        }
+        else if(this.configuredOS == "mac"){
+            
+        }
+    }
 }
---
+++ Creational Design Patterns/Factory Method/example_2.ts
@@ -0,0 +1,97 @@
+export interface Notification {
+
+    render(title, text, thumbnailURL): void;
+    onClick(callback): void;
+    onClosed(callback): void;
+
+}
+
+export class WindowsNotification implements Notification {
+
+    public render(title, text, thumbnailURL) {
+        console.log("Windows Notification Displayed with following information: ", title);
+        console.log("Text: ", text);
+        console.log("URL: ", thumbnailURL);
+    }
+    public onClick(callback): void {
+        console.log("Windows Notification clicked");
+        callback();
+    }
+    public onClosed(callback): void {
+        console.log("Windows Notification Closed");
+        callback();
+    }
+}
+
+export class MacOSNotification implements Notification {
+
+    public render(title, text, thumbnailURL) {
+        console.log("MacOS Notification Displayed with following information: ", title);
+        console.log("Text: ", text);
+        console.log("URL: ", thumbnailURL);
+    }
+    public onClick(callback): void {
+        console.log("MacOS Notification clicked");
+        callback();
+    }
+    public onClosed(callback): void {
+        console.log("MacOS Notification Closed");
+        callback();
+    }
+}
+
+export class NotificationCreator {
+
+    private configuredOS: string = "windows";
+    public notification : Notification;
+
+    public createNotification() {
+       if(this.configuredOS == "windows"){
+        this.notification = new WindowsNotification();
+       }
+       else if(this.configuredOS == "mac"){
+        this.notification = new MacOSNotification();
+       }
+       
+       return this.notification;
+    }
+}
+
+export class NotificationService {
+    
+    private configuredOS = "windows";
+    public notificationCreator: NotificationCreator;
+    
+    constructor(notificationFactory: NotificationCreator){
+        this.notificationCreator = notificationFactory;
+    }
+
+    private greetUser(){
+        let notification: Notification = this.notificationCreator.createNotification();
+
+        let title = "Welcome !";
+        let text = "Let's take a tour";
+        let thumbnailURL = "../assets/welcome.png";
+
+        notification.render(title, text, thumbnailURL);
+        
+        notification.onClick(()=>{
+            console.log("Notification was clicked");
+            
+        });
+        
+        notification.onClosed(()=>{
+            console.log("Notification was closed");
+        });
+    }
+}
+
+class MainApplication {
+    private notificationCreator : NotificationCreator;
+    private notificationSvc: NotificationService;
+    constructor(){
+        
+        this.notificationSvc = new NotificationService(this.notificationCreator);
+    }
+}
+
